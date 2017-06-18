ef3ad776a62c71d211f70b7f4c0fc275699e7921
Anthony Hsia <ahsia3@student.gsu.edu>
2017-06-18 19:16:05 -0400

    Add files via upload

---
+++ Files/src/main/Game.java
@@ -0,0 +1,100 @@
+package main;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.util.Random;
+
+public class Game extends Canvas implements Runnable{
+
+	private static final long serialVersionUID = 6691247796639148462L;
+	
+	public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+	
+	private Thread thread;
+	private boolean running = false;
+	
+	private Random r;
+	private Handler handler; 
+	public Game(){
+		handler = new Handler();
+		
+		this.addKeyListener(new KeyInput(handler));
+		
+		new Window(WIDTH, HEIGHT, "Let's Build a Game!", this);
+		
+		r = new Random();
+		
+		handler.addObject(new Player(WIDTH/2-32, HEIGHT/2-32, ID.Player));
+		handler.addObject(new Player(WIDTH/2+64, HEIGHT/2-32, ID.Player2));
+	}
+	
+	public synchronized void start(){
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+	}
+	
+	public synchronized void stop(){
+		try{
+			thread.join();
+			running = false;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void run(){
+		long lastTime = System.nanoTime();
+		double amountofTicks = 60.0;
+		double ns = 1000000000 / amountofTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running){
+			long now = System.nanoTime();
+			delta += (now - lastTime)/ns;
+			lastTime = now;
+			while(delta >=1){
+				tick();
+				delta--;
+			}
+			if(running)
+				render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();
+	}
+	
+	private void tick(){
+		handler.tick();
+	}
+	
+	private void render(){
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null){
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.cyan);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		handler.render(g);
+		g.dispose();
+		bs.show();
+	}
+	
+	public static void main(String args[]){
+		new Game();
+	}
+}
---
+++ Files/src/main/KeyInput.java
@@ -0,0 +1,65 @@
+package main;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyInput extends KeyAdapter{
+
+	private Handler handler;
+	
+	public KeyInput(Handler handler){
+		this.handler = handler;
+	}
+	
+	public void keyPressed(KeyEvent e){
+		int key = e.getKeyCode();      //when pressed key on keyboard it will display the number value corresponding key pressed
+		
+		for(int i=0; i < handler.object.size(); i++){
+			GameObject tempObject = handler.object.get(i);
+			
+			if(tempObject.getID()==ID.Player){
+				//key events for player 1
+				
+				if (key == KeyEvent.VK_W) tempObject.setVelY(-5);
+				if (key == KeyEvent.VK_S) tempObject.setVelY(5);
+				if (key == KeyEvent.VK_D) tempObject.setVelX(5);
+				if (key == KeyEvent.VK_A) tempObject.setVelX(-5);
+			}
+			
+			if(tempObject.getID()==ID.Player2){
+				//key events for player 2
+
+				if (key == KeyEvent.VK_UP) tempObject.setVelY(-5);
+				if (key == KeyEvent.VK_DOWN) tempObject.setVelY(5);
+				if (key == KeyEvent.VK_RIGHT) tempObject.setVelX(5);
+				if (key == KeyEvent.VK_LEFT) tempObject.setVelX(-5);
+			}
+		}
+	}
+	
+	public void keyReleased(KeyEvent e){
+		int key = e.getKeyCode();
+		for(int i=0; i < handler.object.size(); i++){
+			GameObject tempObject = handler.object.get(i);
+			
+			if(tempObject.getID()==ID.Player){
+				//key events for player 1
+				
+				if (key == KeyEvent.VK_W) tempObject.setVelY(0);
+				if (key == KeyEvent.VK_S) tempObject.setVelY(0);
+				if (key == KeyEvent.VK_D) tempObject.setVelX(0);
+				if (key == KeyEvent.VK_A) tempObject.setVelX(0);
+			}
+			
+			if(tempObject.getID()==ID.Player2){
+				//key events for player 2
+
+				if (key == KeyEvent.VK_UP) tempObject.setVelY(0);
+				if (key == KeyEvent.VK_DOWN) tempObject.setVelY(0);
+				if (key == KeyEvent.VK_RIGHT) tempObject.setVelX(0);
+				if (key == KeyEvent.VK_LEFT) tempObject.setVelX(0);
+			}
+		}
+	}
+	
+}
