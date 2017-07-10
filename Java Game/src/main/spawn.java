2c1585f08154033f4bcfe82b41f1b7bd27342bd1
ahsia3 <ahsia3@student.gsu.edu>
2017-07-10 01:20:43 -0400

    Update

--- Java Game/src/main/spawn.java
+++ Java Game/src/main/spawn.java
@@ -28,16 +28,16 @@ public class spawn {
 			if(game.diff==0){
 				if(hud.getLevel()==2){
 					handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));
-			//	}else if(hud.getLevel() == 3){
-			//		handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));
-			//	}else if(hud.getLevel() == 4){
-			//		handler.addObject(new FastEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.FastEnemy, handler));
-			//	}else if(hud.getLevel() == 5){
-			//		handler.addObject(new SmartEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.SmartEnemy, handler));
-			//	}else if(hud.getLevel() == 6){
-			//		handler.addObject(new FastEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.FastEnemy, handler));
-			//	}else if(hud.getLevel() == 7){
-			//		handler.addObject(new FastEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.FastEnemy, handler));
+				}else if(hud.getLevel() == 3){
+					handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.BasicEnemy, handler));
+				}else if(hud.getLevel() == 4){
+					handler.addObject(new FastEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.FastEnemy, handler));
+				}else if(hud.getLevel() == 5){
+					handler.addObject(new SmartEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.SmartEnemy, handler));
+				}else if(hud.getLevel() == 6){
+					handler.addObject(new FastEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.FastEnemy, handler));
+				}else if(hud.getLevel() == 7){
+					handler.addObject(new FastEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT - 50), ID.FastEnemy, handler));
 				}else if(hud.getLevel() == 3){
 					handler.clearEnemies();
 					handler.addObject(new EnemyBoss((Game.WIDTH/2)-48, -120, ID.EnemyBoss, handler));
